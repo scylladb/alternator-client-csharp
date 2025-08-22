9d95abbffb8eed41a185e6a184590e5c5172ba8c
Dmitry Kropachev <dmitry.kropachev@gmail.com>
2025-08-22 11:51:04 -0400

    1

--- AlternatorLiveNodes.cs
+++ AlternatorLiveNodes.cs
@@ -1,7 +1,3 @@
-// <copyright file="AlternatorLiveNodes.cs" company="PlaceholderCompany">
-// Copyright (c) PlaceholderCompany. All rights reserved.
-// </copyright>
-
 namespace ScyllaDB.Alternator
 {
     using System;
